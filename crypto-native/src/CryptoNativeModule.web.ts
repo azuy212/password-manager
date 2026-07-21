32fb6fb0d4b58bb1a3d10fa08b83b5551546e6f5
Ali Zain Ul Yasoob <ali.zain212@yahoo.com>
2026-07-21 14:39:01 +0500

    fix: add explicit return types to CryptoNativeModule.web.ts methods

--- crypto-native/src/CryptoNativeModule.web.ts
+++ crypto-native/src/CryptoNativeModule.web.ts
@@ -4,47 +4,47 @@ import type { CryptoNativeModuleEvents } from './CryptoNative.types';
 import * as impl from './cryptoImpl';
 
 class CryptoNativeModule extends NativeModule<CryptoNativeModuleEvents> {
-  generateSalt(length: number) {
+  generateSalt(length: number): number[] {
     return impl.generateSalt(length);
   }
 
-  deriveKey(password: string, salt: number[], iterations: number, keyLength: number) {
+  deriveKey(password: string, salt: number[], iterations: number, keyLength: number): Promise<number[]> {
     return impl.deriveKey(password, salt, iterations, keyLength);
   }
 
-  encrypt(data: number[], keyBytes: number[]) {
+  encrypt(data: number[], keyBytes: number[]): Promise<impl.EncryptionResult> {
     return impl.encrypt(data, keyBytes);
   }
 
-  decrypt(ciphertext: number[], keyBytes: number[], nonce: number[], tag: number[]) {
+  decrypt(ciphertext: number[], keyBytes: number[], nonce: number[], tag: number[]): Promise<number[]> {
     return impl.decrypt(ciphertext, keyBytes, nonce, tag);
   }
 
-  generateKeyPair() {
+  generateKeyPair(): impl.KeyPair {
     return impl.generateKeyPair();
   }
 
-  sign(data: number[], privateKeyBytes: number[]) {
+  sign(data: number[], privateKeyBytes: number[]): number[] {
     return impl.sign(data, privateKeyBytes);
   }
 
-  verify(data: number[], signatureBytes: number[], publicKeyBytes: number[]) {
+  verify(data: number[], signatureBytes: number[], publicKeyBytes: number[]): boolean {
     return impl.verify(data, signatureBytes, publicKeyBytes);
   }
 
-  generateX25519KeyPair() {
+  generateX25519KeyPair(): impl.X25519KeyPair {
     return impl.generateX25519KeyPair();
   }
 
-  ecdh(privateKey: number[], publicKey: number[]) {
+  ecdh(privateKey: number[], publicKey: number[]): number[] {
     return impl.ecdh(privateKey, publicKey);
   }
 
-  hmacSha256(data: number[], keyBytes: number[]) {
+  hmacSha256(data: number[], keyBytes: number[]): Promise<number[]> {
     return impl.hmacSha256(data, keyBytes);
   }
 
-  generateRandomBytes(length: number) {
+  generateRandomBytes(length: number): number[] {
     return impl.generateRandomBytes(length);
   }
 }
